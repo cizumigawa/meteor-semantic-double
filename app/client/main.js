c4adb5d09b2bfd4c91ed0240a3285f499fc2e9d3
Christianne <cizumigawa@dhcp-168-105-254-130.wireless.manoa.hawaii.edu>
2017-10-12 09:43:42 -1000

    no message

---
+++ app/client/main.js
@@ -0,0 +1,34 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  thatsAlot() {
+    if (Template.instance().counter.get() > 5) {
+      Template.instance.words.set('That’s a lot!');
+    }
+
+    else {
+      Template.instance.words.set('');
+    }
+  },
+
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+
